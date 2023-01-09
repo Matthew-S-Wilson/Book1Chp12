689b7f7bcb0fa29933b61f86313d09961f3c7d4c
Matthew-S-Wilson <matthew255@gmail.com>
2023-01-09 11:17:25 -0600

    first commit

---
+++ main.js
@@ -0,0 +1,37 @@
+
+const gameboy = {
+    id: 1,
+    name: "Gameboy",
+    maker: "Nintendo",
+    purchased: true,
+    price: 100,
+
+}
+
+const Cookbook = {
+    id: 2,
+    name: "Cooking Everything",
+    maker: "Bama Books",
+    purchased: false,
+    price: 30,
+
+}
+
+const slingshot = {
+    id: 3,
+    name: "Slingshot",
+    maker: "Warner Toys",
+    purchased: true,
+    price: 5,
+
+}
+
+
+// each toy or gift has a string property for the id, a string property for the name, a string property for the maker, a boolean proprty for purchased,
+// and an interger proprty for the price
+
+//forgot to add comma after each property in the variable and got hung up for a while; be exact !
+
+
+
+console.log(slingshot)
